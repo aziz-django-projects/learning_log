04215b89988a8c479ca8b91dd231652e33d4da8a
zihinbender <budakaziz@gmail.com>
2026-01-20 12:25:27 +0300

    settngs revised

--- ll_project/settings/base.py
+++ ll_project/settings/base.py
@@ -61,17 +61,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 ]
 
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
-
 ROOT_URLCONF = "ll_project.urls"
 
 TEMPLATES = [
