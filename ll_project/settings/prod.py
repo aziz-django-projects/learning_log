ee99dd2c638d68719d8ac8263c2dc8084339a37f
zihinbender <73230187+zihinbender@users.noreply.github.com>
2026-01-20 14:45:13 +0300

    Update prod.py

--- ll_project/settings/prod.py
+++ ll_project/settings/prod.py
@@ -22,7 +22,7 @@ STORAGES = {
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 DATABASES = {
     "default": dj_database_url.config(
-        default=None,
+        default=os.getenv("DATABASE_URL", "sqlite:///db.sqlite3"),
         conn_max_age=600,
         ssl_require=True,
     )
