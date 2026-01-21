4e77a27adddb25da8569bde7fdcc76bf76477e6b
zihinbender <73230187+zihinbender@users.noreply.github.com>
2026-01-21 15:18:33 +0300

    Update prod.py

--- ll_project/settings/prod.py
+++ ll_project/settings/prod.py
@@ -42,17 +42,3 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {"console": {"class": "logging.StreamHandler"}},
-    "root": {"handlers": ["console"], "level": "INFO"},
-    "loggers": {
-        "django.request": {
-            "handlers": ["console"],
-            "level": "ERROR",
-            "propagate": False,
-        },
-    },
-}
-
